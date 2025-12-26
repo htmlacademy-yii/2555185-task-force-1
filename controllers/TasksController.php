2ed8f334f6f7faf371ccc0b25ba23ff7f5dde4e3
Alexmasya97 <sasa_97@mail.ru>
2025-12-26 21:36:33 +1000

    add task-contriller n view

---
+++ controllers/TasksController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+use app\models\Task;
+
+class TasksController extends Controller
+{
+    public function actionIndex()
+    {
+        $tasks = Task::find()
+            ->where(['status' => Task::NEW])
+            ->with(['city', 'category'])
+            ->orderBy(['date_add' => SORT_DESC])
+            ->all();
+
+        return $this->render('index', [
+            'tasks' => $tasks
+        ]);
+    }
+}
--- models/Task.php
+++ models/Task.php
@@ -29,7 +29,11 @@ use Yii;
  */
 class Task extends \yii\db\ActiveRecord
 {
-
+    const NEW = 'new';
+    const CANCELED = 'canceled';
+    const IN_PROGRESS = 'in_progress';
+    const COMPLETED = 'completed';
+    const FAILED = 'failed';
 
     /**
      * {@inheritdoc}
